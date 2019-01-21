6f2405dc731ebc3e893f8ea7ba81ddf96a2adc46
spider65 <raniero.albanesi@gmail.com>
2019-01-21 12:56:47 +0100

    Add files via upload
    aggiunti codici per controllo motori e lettura encoder.

---
+++ encoder.py
@@ -0,0 +1,56 @@
+from RPi import GPIO
+from time import sleep
+
+clk_e = 17
+dt_e = 18
+
+clk_w = 22
+dt_w= 23
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(clk_e, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(dt_e, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(clk_w, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(dt_w, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+def encoder_est
+    counter = 0
+    clkLastState_e = GPIO.input(clk_e)
+
+
+    try:
+
+            while True:
+                    clkState_e = GPIO.input(clk_e)
+                    dtState_e = GPIO.input(dt_e)
+                    if clkState_e != clkLastState_e:
+                            if dtState_e != clkState_e:
+                                    counter += 1
+                            else:
+                                    counter -= 1
+                            print counter
+                    clkLastState_e = clkState_e
+                    sleep(0.01)
+
+def encoder_west
+    counter = 0
+    clkLastState_w = GPIO.input(clk_w)                
+    try:
+
+            while True:
+                    clkState_w = GPIO.input(clk_w)
+                    dtState_w = GPIO.input(dt_w)
+                    if clkState_w != clkLastState_w:
+                            if dtState_w != clkState_w:
+                                    counter += 1
+                            else:
+                                    counter -= 1
+                            print counter
+                    clkLastState_w = clkState_w
+                    sleep(0.01)                
+                
+                
+                
+                
+finally:
+GPIO.cleanup()
---
+++ motorControl.py
@@ -0,0 +1,52 @@
+import RPi.GPIO as GPIO
+from time import sleep
+ 
+GPIO.setmode(GPIO.BOARD)
+ 
+MotorE_A = 6
+MotorE_B = 13
+MotorE_E = 5
+ 
+MotorW_A = 20
+MotorW_B = 21
+MotorW_E = 16
+ 
+GPIO.setup(MotorE_A,GPIO.OUT)
+GPIO.setup(MotorE_B,GPIO.OUT)
+GPIO.setup(MotorE_E,GPIO.OUT)
+ 
+GPIO.setup(MotorW_A,GPIO.OUT)
+GPIO.setup(MotorW_B,GPIO.OUT)
+GPIO.setup(MotorW_E,GPIO.OUT)
+ 
+
+def go_in_open_motor_e
+    GPIO.output(MotorE_A,GPIO.HIGH)
+    GPIO.output(MotorE_B,GPIO.LOW)
+    GPIO.output(MotorE_E,GPIO.HIGH)
+    sleep(2)
+def go_in_open_motor_w     
+    GPIO.output(MotorW_A,GPIO.HIGH)
+    GPIO.output(MotorW_B,GPIO.LOW)
+    GPIO.output(MotorW_E,GPIO.HIGH)
+    sleep(2)
+ 
+def go_in_closed_motor_e
+    GPIO.output(MotorE_A,GPIO.LOW)
+    GPIO.output(MotorE_B,GPIO.HIGH)
+    GPIO.output(MotorE_E,GPIO.HIGH)
+    sleep(2)
+    
+def go_in_closed_motor_w     
+    GPIO.output(MotorW_A,GPIO.LOW)
+    GPIO.output(MotorW_B,GPIO.HIGH)
+    GPIO.output(MotorW_E,GPIO.HIGH) 
+    sleep(2)
+ 
+def stop_motor_e
+    GPIO.output(MotorE_E,GPIO.LOW)
+    
+def stop_motor_w    
+    GPIO.output(MotorW_E,GPIO.LOW)
+ 
+GPIO.cleanup()
