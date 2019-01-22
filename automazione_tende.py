131ee5db3f3612b1de5fab41195bddcd9e9c89ea
spider65 <raniero.albanesi@gmail.com>
2019-01-22 13:13:57 +0100

    Add files via upload
    modifica metodo controllo tende e metodo park

--- automazione_tende.py
+++ automazione_tende.py
@@ -26,63 +26,79 @@ def read_curtains_height():
 
     pass
 
-def move_curtains_height(coord):
+def move_curtains_height(prevCoord,coord):
 
     """Muovi l'altezza delle tende"""
 
     condition_e = None # da calcolare
     condition_w = None # da calcolare
     # TODO verifica altezza del tele:
+    
+    # telescopio sopra tenda Est
+    # alza completamente tenda West
+    if azSE > (coord['az']) > azNE:
+        # h_tenda_W = max
+        motor_control.go_in_open_motor_w()
+        if encoder.encoder_west(condition_w) == 'Stop':
+            motor_control.stop_motor_w()
+        #h_tenda_E = (coord['alt']) - x #x = parametro di sicurezza
+        if prevCoord['alt'] > coord['alt']:
+            motor_control.go_in_closed_motor_e()
+            # inserire controllo posizione encoder
+        else:
+            motor_control.go_in_open_motor_e()
+            # inserire controllo posizione encoder   
+    if altTele < y: #y altezza minima puntamento telescopio
+        # h_tenda_E = 0
+        motor_control.go_in_closed_motor_e()
+        if encoder.encoder_est(condition_e) == 'Stop':
+            motor_control.stop_motor_e()
+    
+    # telescopio sopra tenda West
+    # alza completamente la tendina est
+    if azNW > (coord['az']) > azSW:
+        #h_tenda_E = max  
+        motor_control.go_in_open_motor_e()
+        if encoder.encoder_est(condition_e) == 'Stop':
+            motor_control.stop_motor_e()
+        # h_tenda_W = (coord['alt']) - x
+        if prevCoord['alt'] > coord['alt']:
+            motor_control.go_in_closed_motor_w()
+            # inserire controllo posizione encoder
+        else: 
+            motor_control.go_in_open_motor_w()
+                    
+    if (coord['az']) < y:
+        #h_tenda_W = 0
+        motor_control.go_in_closed_motor_w()
+        if encoder.encoder_west(condition_w) == 'Stop':
+            motor_control.stop_motor_w()
+    
     # if inferiore a est_min_height e ovest_min_height
-    #   muovi entrambe le tendine a 0
-    motor_control.go_in_closed_motor_e()
-    if encoder.encoder_est(condition_e) == 'Stop':
-        motor_control.stop_motor_e()
-
-    motor_control.go_in_closed_motor_w()
-    if encoder.encoder_west(condition_w) == 'Stop':
-        motor_control.stop_motor_w()
-
+    # muovi entrambe le tendine a 0
+    if (coord['az']) = az_park_tend_e and (coord['alt']) = alt_park_tend_e: #az_park_tend e alt_park_tend valori di az e alt tende in posozione chiusura
+        # h_tenda_E = 0
+        motor_control.go_in_closed_motor_e()
+        if encoder.encoder_est(condition_e) == 'Stop':
+            motor_control.stop_motor_e()
+        # h_tenda_W = 0 
+        motor_control.go_in_closed_motor_w()
+        if encoder.encoder_west(condition_w) == 'Stop':
+            motor_control.stop_motor_w()
+   
     # else if superiore a est_max_height e ovest_max_height
-    #   entrambe le tendine completamente alzate
-    motor_control.go_in_open_motor_e()
-    if encoder.encoder_est(condition_e) == 'Stop':
-        motor_control.stop_motor_e()
-
-    motor_control.go_in_open_motor_w()
-    if encoder.encoder_west(condition_w) == 'Stop':
-        motor_control.stop_motor_w()
-
-    # else if superiore a est_min_height e azimut del tele a ovest
-    # else if azimut compreso tra azSE e azNE: (qui bisognerà fare dei conti differenziali perchè passiamo lo 0 dell'azimut e giriamo in senso oriario)
-    #   alza completamente la tendina est
-    motor_control.go_in_open_motor_e()
-    if encoder.encoder_est(condition_e) == 'Stop':
-        motor_control.stop_motor_e()
-
-    #   if inferiore a ovest_min_height
-    #     muovi la tendina ovest a 0
-    motor_control.go_in_closed_motor_w()
-    if encoder.encoder_west(condition_w) == 'Stop':
-        motor_control.stop_motor_w()
-
-    #   else
-    #     muovi la tendina ovest a f(altezza telescopio - x)
-    # else if superiore a ovest_min_height e azimut del tele a est
-    #   alza completamente la tendina est
-    motor_control.go_in_open_motor_e()
-    if encoder.encoder_est(condition_e) == 'Stop':
-        motor_control.stop_motor_e()
-    #   if inferiore a est_min_height
-    #     muovi la tendina est a 0
-
-    motor_control.go_in_closed_motor_e()
-    if encoder.encoder_est(condition_e) == 'Stop':
-        motor_control.stop_motor_e()
-    #   else
-    #     muovi la tendina est a f(altezza telescopio - x)
-
-    pass
+    # entrambe le tendine completamente alzate
+    if azNE > (coord['alt']) > 0 or azSW > (coord['alt']) > azSE or 360 > (coord['alt']) > azNW or ((coord['alt']) > alt_max_tend_e and (coord['alt']) > alt_max_tend_w):
+        # h_tenda_E = max        
+        motor_control.go_in_open_motor_e()
+        if encoder.encoder_est(condition_e) == 'Stop':
+            motor_control.stop_motor_e()
+        # h_tenda_W = max
+        motor_control.go_in_open_motor_w()
+        if encoder.encoder_west(condition_w) == 'Stop':
+            motor_control.stop_motor_w()
+        
+     pass
 
 def park_curtains():
 
@@ -92,11 +108,11 @@ def park_curtains():
     condition_w = None # da calcolare
 
     # TODO muovi i motori portando a zero le tendine
-    motor_control.go_in_open_motor_e()
+    motor_control.go_in_closed_motor_e()
     if encoder.encoder_est(condition_e) == 'Stop':
             motor_control.stop_motor_e()
 
-    motor_control.go_in_open_motor_w()
+    motor_control.go_in_closed_motor_w()
     if encoder.encoder_west(condition_w) == 'Stop':
             motor_control.stop_motor_w()
     return { 'alt': "0", 'az': None}
