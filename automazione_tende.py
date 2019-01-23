b35b3f0fd4cfde64943d57dc5d9a8e664123f95a
Alessio Caradossi <alessio.caradossi@dxc.com>
2019-01-23 13:45:17 +0100

    fix alcuni problemi di sintassi e indentazione

--- automazione_tende.py
+++ automazione_tende.py
@@ -33,15 +33,15 @@ def move_curtains_height(prevCoord,coord):
     alt_max_tend_w = config.Config.getValue("tenda_max_west")
     alt_min_tel_e = config.Config.getValue("alt_min_tel_e")
     alt_min_tel_w = config.Config.getValue("alt_min_tel_w")
-    
+
     # stabilisco il valore di increm per ogni tenda, increm corrisponde al valore dell'angolo della tenda coperto da 1 step)
     increm_e = (config.Config.getValue("tenda_max_est")-config.Config.getValue("tenda_park_est"))/config.Config.getValue('n_step_corsa_tot')
     increm_w = (config.Config.getValue("tenda_max_west")-config.Config.getValue("tenda_park_west"))/config.Config.getValue('n_step_corsa_tot')
-    
+
     condition_e = None # da calcolare
     condition_w = None # da calcolare
     # TODO verifica altezza del tele:
-    
+
     # telescopio sopra tenda Est
     # alza completamente tenda West
     if azSE > (coord['az']) > azNE:
@@ -49,7 +49,7 @@ def move_curtains_height(prevCoord,coord):
         motor_control.go_in_open_motor_w() # chiamo il comando per attivazione motore verso apertura
         if encoder.encoder_west(condition_w) == 'Stop': # controllo condizione encoder
             motor_control.stop_motor_w() # chiamo il comando per lo stop del motore
-            
+
         # controllo h_tenda_E = (coord['alt']) - x # x = parametro di sicurezza
         if prevCoord['alt'] > coord['alt']:
             if coord['alt'] <= alt_min_tel_e: #y altezza minima puntamento telescopio
@@ -62,10 +62,10 @@ def move_curtains_height(prevCoord,coord):
                 move_for_step = delta_coord/increm_e # calcolo gli step corrispondenti al delta_coord
                 last_encoder_est_condition = encoder.encoder_est(condition) # leggo l'ultima posizione dell'encoder
                 motor_control.go_in_closed_motor_e() # chiamo la funzione di start del motore verso chiusura
-                    if encoder.encoder_est(condition) <= last_encoder_est_condition - move_for_step): # comparo la lettura dell'encoder con il valore raggiungere
-                        motor_control.stop_motor_e()  # chiamo il comando per lo stop del motore
-                                
-                   
+                if encoder.encoder_est(condition) <= last_encoder_est_condition - move_for_step: # comparo la lettura dell'encoder con il valore raggiungere
+                    motor_control.stop_motor_e()  # chiamo il comando per lo stop del motore
+
+
         else:
             #controllo altezza minima puntamento telescopio
             if coord['alt'] <= alt_min_tel_e: #y altezza minima puntamento telescopio
@@ -77,12 +77,12 @@ def move_curtains_height(prevCoord,coord):
                 delta_coord= coord['alt']-prevCoord['alt'] # calcolo la differenza del valore di altezza tra le ultime due posizioni del tele
                 move_for_step = delta_coord/increm_e # calcolo gli step corrispondenti al delta_coord
                 last_encoder_est_condition = encoder.encoder_est(condition) # leggo l'ultima posizione dell'encoder
-                motor_control.go_in_open_motor_e() # chiamo la funzione di start del motore verso apertura   
-                    if encoder.encoder_est(condition) => last_encoder_est_condition + move_for_step): # comparo la lettura dell'encoder con il valore da raggiungere
-                        motor_control.stop_motor_e()  # chiamo il comando per lo stop del motore
-                
-               
-    
+                motor_control.go_in_open_motor_e() # chiamo la funzione di start del motore verso apertura
+                if encoder.encoder_est(condition) >= last_encoder_est_condition + move_for_step: # comparo la lettura dell'encoder con il valore da raggiungere
+                    motor_control.stop_motor_e()  # chiamo il comando per lo stop del motore
+
+
+
     # telescopio sopra tenda West
     # alza completamente la tendina est
     if azNW > (coord['az']) > azSW:
@@ -90,7 +90,7 @@ def move_curtains_height(prevCoord,coord):
         motor_control.go_in_open_motor_e() # chiamo il comando per attivazione motore verso apertura
         if encoder.encoder_est(condition_e) == 'Stop': # controllo condizione encoder
             motor_control.stop_motor_e() # chiamo il comando per lo stop del motore
-            
+
         # controllo h_tenda_W = (coord['alt']) - x # x = parametro di sicurezza
         if prevCoord['alt'] > coord['alt']:
             if coord['alt'] <= alt_min_tel_w: #y altezza minima puntamento telescopio
@@ -103,10 +103,10 @@ def move_curtains_height(prevCoord,coord):
                 move_for_step = delta_coord/increm_w # calcolo gli step corrispondenti al delta_coord
                 last_encoder_west_condition = encoder.encoder_west(condition) # leggo l'ultima posizione dell'encoder
                 motor_control.go_in_closed_motor_w() # chiamo la funzione di start del motore verso chiusura
-                    if encoder.encoder_west(condition) <= last_encoder_west_condition - move_for_step): # comparo la lettura dell'encoder con il valore raggiungere
-                        motor_control.stop_motor_w()  # chiamo il comando per lo stop del motore
-                                
-                   
+                if encoder.encoder_west(condition) <= last_encoder_west_condition - move_for_step: # comparo la lettura dell'encoder con il valore raggiungere
+                    motor_control.stop_motor_w()  # chiamo il comando per lo stop del motore
+
+
         else:
             #controllo altezza minima puntamento telescopio
             if coord['alt'] <= alt_min_tel_w: #y altezza minima puntamento telescopio
@@ -118,11 +118,11 @@ def move_curtains_height(prevCoord,coord):
                 delta_coord= coord['alt']-prevCoord['alt'] # calcolo la differenza del valore di altezza tra le ultime due posizioni del tele
                 move_for_step = delta_coord/increm_w # calcolo gli step corrispondenti al delta_coord
                 last_encoder_west_condition = encoder.encoder_west(condition) # leggo l'ultima posizione dell'encoder
-                motor_control.go_in_open_motor_w() # chiamo la funzione di start del motore verso apertura   
-                    if encoder.encoder_west(condition) => last_encoder_west_condition + move_for_step): # comparo la lettura dell'encoder con il valore da raggiungere
-                        motor_control.stop_motor_w()  # chiamo il comando per lo stop del motore
-         
-    
+                motor_control.go_in_open_motor_w() # chiamo la funzione di start del motore verso apertura
+                if encoder.encoder_west(condition) >= last_encoder_west_condition + move_for_step: # comparo la lettura dell'encoder con il valore da raggiungere
+                    motor_control.stop_motor_w()  # chiamo il comando per lo stop del motore
+
+
     # if inferiore a est_min_height e ovest_min_height
     # muovi entrambe le tendine a 0
     if (coord['alt']) <= az_park_tend_e and (coord['alt']) <= alt_park_tend_w: #az_park_tend e alt_park_tend valori di az e alt tende in posizione chiusura
@@ -130,15 +130,15 @@ def move_curtains_height(prevCoord,coord):
         motor_control.go_in_closed_motor_e()
         if encoder.encoder_est(condition_e) == 'Stop':
             motor_control.stop_motor_e()
-        # h_tenda_W = 0 
+        # h_tenda_W = 0
         motor_control.go_in_closed_motor_w()
         if encoder.encoder_west(condition_w) == 'Stop':
             motor_control.stop_motor_w()
-   
+
     # else if superiore a est_max_height e ovest_max_height
     # entrambe le tendine completamente alzate
     if azNE > (coord['alt']) > 0 or azSW > (coord['alt']) > azSE or 360 > (coord['alt']) > azNW or ((coord['alt']) > alt_max_tend_e and (coord['alt']) > alt_max_tend_w):
-        # h_tenda_E = max        
+        # h_tenda_E = max
         motor_control.go_in_open_motor_e()
         if encoder.encoder_est(condition_e) == 'Stop':
             motor_control.stop_motor_e()
@@ -146,8 +146,6 @@ def move_curtains_height(prevCoord,coord):
         motor_control.go_in_open_motor_w()
         if encoder.encoder_west(condition_w) == 'Stop':
             motor_control.stop_motor_w()
-        
-     pass
 
 def park_curtains():
 
