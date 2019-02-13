1f419e2ca13f8c3c4983921dd0fef96e4e52496c
Raniero Albanesi <raniero@raniero-OptiPlex-GX270>
2019-02-13 11:30:22 +0100

    codice per il controllo dello stato e dei movimenti del tetto

---
+++ roof_control.py
@@ -0,0 +1,31 @@
+import config
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+roof_verify_closed = config.Config.getValue("roof_verify_open", 'roof_board') # 11
+roof_verify_open = config.Config.getValue("roof_verify_open", 'roof_board') # 7
+roof_open = config.Config.getValue("roof_open", 'roof_board') # 7
+roof_closed = config.Config.getValue("roof_closed", 'roof_board') # 15
+
+GPIO.setup(roof_verify_closed, GPIO.IN, pull_up-down=GPIO.PUD_UP)
+GPIO.setup(roof_verify_open, GPIO.IN, pull_up-down=GPIO.PUD_UP)
+GPIO.setup(roof_open,GPIO.OUT)
+GPIO.setup(roof_closed,GPIO.OUT)
+
+def verify_closed_roof():
+    GPIO.wait_for_edge(roof_verify_closed, GPIO.FALLING) # verifica che lo stato del pin sia alto, e restituisce O quando questo cambia alla pressione dell'interruttore
+    sleep(config.Config.getFloat("sleep"))
+
+def verify_open_roof():
+    GPIO.wait_for_edge(roof_verify_open, GPIO.FALLING) # verifica che lo stato del pin sia alto, e restituisce O quando questo cambia alla pressione dell'interruttore
+    sleep(config.Config.getFloat("sleep"))
+
+def open_roof():
+    GPIO.output(roof_open,GPIO.HIGH)
+    sleep(config.Config.getFloat("sleep"))
+
+def closed_roof():
+    GPIO.output(roof_closed,GPIO.HIGH)
+    sleep(config.Config.getFloat("sleep"))
+
