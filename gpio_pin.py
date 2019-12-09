315a01d39ab6225b47b16a1681efdf5b49cd3b47
Alessio Caradossi <acaradossi@hotels.com>
2019-12-09 22:23:54 +0100

    added all used GPIO

--- gpio_pin.py
+++ gpio_pin.py
@@ -8,6 +8,18 @@ class GPIOPin(Enum):
     VERIFY_OPEN = (Config.getValue("roof_verify_open", "roof_board"), GPIO.IN, GPIO.PUD_UP)
     SWITCH_ROOF = (Config.getValue("switch_roof", "roof_board"), GPIO.IN, GPIO.PUD_UP)
 
+    MOTORE_A = (Config.getValue("motorE_A", "motor_board"), GPIO.OUT)
+    MOTORE_B = (Config.getValue("motorE_B", "motor_board"), GPIO.OUT)
+    MOTORE_E = (Config.getValue("motorE_E", "motor_board"), GPIO.OUT)
+    MOTORW_A = (Config.getValue("motorW_A", "motor_board"), GPIO.OUT)
+    MOTORW_B = (Config.getValue("motorW_B", "motor_board"), GPIO.OUT)
+    MOTORW_E = (Config.getValue("motorW_E", "motor_board"), GPIO.OUT)
+
+    CLK_E = (Config.getValue("clk_e", "encoder_board"), GPIO.IN, GPIO.PUD_UP)
+    DT_E = (Config.getValue("dt_e", "encoder_board"), GPIO.IN, GPIO.PUD_UP)
+    CLK_W = (Config.getValue("clk_w", "encoder_board"), GPIO.IN, GPIO.PUD_UP)
+    DT_W = (Config.getValue("dt_w", "encoder_board"), GPIO.IN, GPIO.PUD_UP)
+
     def __init__(self, id_pin, setup, pull=None):
         self.id_pin = id_pin
         self.setup = setup
