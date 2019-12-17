aef620e446662e4e176417bea08cf1d5318b0cde
Alessio Caradossi <acaradossi@hotels.com>
2019-12-17 21:20:35 +0100

    pass id_pin attribute as a pin number

--- gpio_config.py
+++ gpio_config.py
@@ -9,18 +9,18 @@ class GPIOConfig(metaclass=Singleton):
         GPIOPin.setup()
 
     def turn_on(self, switch):
-        GPIO.output(GPIO.input(switch), GPIO.HIGH)
+        GPIO.output(GPIO.input(switch.id_pin), GPIO.HIGH)
 
     def turn_off(self, switch):
-        GPIO.output(GPIO.input(switch), GPIO.LOW)
+        GPIO.output(GPIO.input(switch.id_pin), GPIO.LOW)
 
     def wait_for_raising(self, switch, timeout=60000):
-        is_finished = GPIO.wait_for_edge(GPIO.input(switch), GPIO.RAISING, timeout=timeout)
+        is_finished = GPIO.wait_for_edge(GPIO.input(switch.id_pin), GPIO.RAISING, timeout=timeout)
         return is_finished != None
 
     def wait_for_falling(self, switch, timeout=60000):
-        is_finished = GPIO.wait_for_edge(GPIO.input(switch), GPIO.FALLING, timeout=timeout)
+        is_finished = GPIO.wait_for_edge(GPIO.input(switch.id_pin), GPIO.FALLING, timeout=timeout)
         return is_finished != None
 
     def status(self, switch):
-        return GPIO.input(switch) == GPIO.HIGH
+        return GPIO.input(switch.id_pin) == GPIO.HIGH
