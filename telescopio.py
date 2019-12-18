c50ad41d8c408cb9509a80df6e5d9327273f8bbf
spider65 <raniero.albanesi@gmail.com>
2019-12-18 16:04:12 +0100

    modifiche di indirizzo e port dovute ad una errata apertura del branch

--- telescopio.py
+++ telescopio.py
@@ -30,4 +30,4 @@ class Telescopio(BaseTelescopio):
         return coords
 
 if __name__ == '__main__':
-    netcat("192.168.1.22", 3040, 'MountGetAltAzi.js')
+    netcat("192.168.1.3", 3040, 'MountGetAltAzi.js')
