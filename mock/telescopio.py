4227489e112bbd2ea316f36703a53107f7374634
spider65 <raniero.albanesi@gmail.com>
2019-12-20 16:21:16 +0100

    correzione nome file js

--- mock/telescopio.py
+++ mock/telescopio.py
@@ -35,4 +35,4 @@ class Telescopio(BaseTelescopio):
 
 
 if __name__ == '__main__':
-    netcat("192.168.0.9", 3040, 'MountGetAltAzi.js', 'SetParkTel.js')
+    netcat("192.168.0.9", 3040, 'MountGetAltAzi.js', 'SetTelPark.js')
