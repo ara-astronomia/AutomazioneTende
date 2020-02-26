2ed6a86dfd84fad471d6a34b16ec22bcfab1d0aa
spider65 <raniero.albanesi@gmail.com>
2020-02-26 08:55:16 +0100

    modified number status of stopped

--- status.py
+++ status.py
@@ -6,13 +6,4 @@ class Status(IntEnum):
     CLOSED = 2
     OPENING = 3
     CLOSING = 4
-<<<<<<< HEAD
-=======
     STOPPED = 5
-
-class CurtainStatus(IntEnum):
-
-    OPENING = 1
-    CLOSING = 2
->>>>>>> 8d417683ab6a749317b538227857e00722659a0e
-    STOPPED = 3
