70c10adaaeed7ceeb6be8f4daced875fb6918b90
Alessio Caradossi <acaradossi@hotels.com>
2020-03-06 13:06:12 +0100

    removed unused config, added danger status check

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ def connection(error, roof, curtains):
         while True:
             ev1, vals1 = g_ui.win.Read(timeout=10000)
 
-            if ev1 is None or ev1 == "exit" or ev1 == 'roof_sim':
+            if ev1 is None or ev1 == "exit":
                 v = "E"
             elif error:
                 continue
@@ -19,9 +19,7 @@ def connection(error, roof, curtains):
 
             elif ev1 == 'park-tele':
                 v = "P"
-                Logger.getLogger().info("telescopio inviato alla posizione di park (park_tele) ")
-                g_ui.update_status_tele('park')
-
+                
             elif ev1 == 'close-roof':
                 if curtains is True:
                     g_ui.roof_alert('Attenzione tendine aperte')
@@ -33,7 +31,6 @@ def connection(error, roof, curtains):
                 if roof is False:
                     g_ui.roof_alert('Attenzione tetto chiuso')
                     continue
-                g_ui.update_status_tele('tracking')
                 g_ui.base_draw()
                 v = "1"
 
@@ -42,6 +39,7 @@ def connection(error, roof, curtains):
 
             elif ev1 == "shutdown":
                 v = "-"
+
             else:
                 if curtains is False:
                     continue
@@ -59,29 +57,23 @@ def connection(error, roof, curtains):
                 roof = False
                 s.close()
                 return "E", None
-            elif ev1 == "roof_sim":
-                g_ui.update_curtains_text(0,0)
-                g_ui.update_curtains_graphic(0,0)
-                curtains = False
-                g_ui.closed_roof("Tetto Chiuso")
-                roof = False
-                if vals1['roof_sim']:
-                    host = config.Config.getValue("roof_sim_ip", "server")  # The server's hostname or IP address
-                    port = config.Config.getInt("roof_sim_port", "server")        # The port used by the server
-                else:
-                    host = config.Config.getValue("ip", "server")  # The server's hostname or IP address
-                    port = config.Config.getInt("port", "server")        # The port used by the server
-                s.close()
-                return "ReConnect", [host, port]
+
             data = rcv.decode("UTF-8")
             Logger.getLogger().debug("Data: "+data)
             if data[0] == "R":
                 if data[-1] == "1":
                     roof = True
                     g_ui.open_roof("Tetto Aperto")
+                    g_ui.update_status_crac('tetto aperto')
                 elif data[-1] == "0":
                     roof = False
                     g_ui.closed_roof("Tetto Chiuso")
+                    g_ui.update_status_crac('tetto chiuso')
+                elif data[-1] == "P":
+                    Logger.getLogger().info("telescopio inviato alla posizione di park (park_tele) ")
+                    g_ui.update_status_crac('tele in park')
+            elif data[0] == "D":
+                g_ui.update_status_crac('controllare switch tendine')
             elif data[0] == "E":
                 # la if si potrebbe togliere, l'errore dovrebbe sempre essere bloccante
                 # ma va testato meglio
@@ -91,8 +83,10 @@ def connection(error, roof, curtains):
             else:
                 if data == "000000":
                     curtains = False
+                    g_ui.update_status_crac('tendine chiuse')
                 else:
                     curtains = True
+                    g_ui.update_status_crac('tendine aperte')
                 alpha_e, alpha_w = g_ui.update_curtains_text(int(data[0:3]), int(data[3:6]))
                 g_ui.update_curtains_graphic(alpha_e, alpha_w)
 
