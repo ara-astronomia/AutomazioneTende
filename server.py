9dcbfce9b074becdfbdb18d06c483313195ae01b
Alessio Caradossi <acaradossi@hotels.com>
2020-03-19 11:22:32 +0100

    fix curtains waiting next loop after enabled

--- server.py
+++ server.py
@@ -33,6 +33,8 @@ try:
                 while True:
                     Logger.getLogger().debug(automazioneTende.crac_status)
                     data: bytes = conn.recv(1)
+                    Logger.getLogger().debug("Data: %s", data)
+                    
                     if not data or (data == b"0" or data == b'E') and automazioneTende.started:
                         automazioneTende.started = False
                         automazioneTende.park_curtains()
@@ -68,8 +70,8 @@ try:
                                 automazioneTende.exit_program()
                                 exit(0)
                             break
-
-                    if data in [b"1", b"c"]:
+                    
+                    if data == b'1' or data == b'c':
                         Logger.getLogger().debug("chiamata al metodo per muovere le tendine (automazioneTende.exec) %s", automazioneTende.started)
                         automazioneTende.exec()
 
