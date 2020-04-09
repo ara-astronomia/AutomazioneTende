d45e0de0d8f82cf7abc6fa9fb3a218f7433750c6
Alessio Caradossi <acaradossi@hotels.com>
2020-04-09 15:15:30 +0200

    #89 removed unused crac_status, fix some conditions

--- checker_client.py
+++ checker_client.py
@@ -2,7 +2,6 @@ import time, config, socket
 from electro_tests import gui
 from gui_constants import GuiKey,GuiLabel
 from logger import Logger
-from crac_status import CracStatus
 from status import Status, TelescopeStatus
 
 def change_status(status_switch, key, win):
@@ -12,7 +11,6 @@ def change_status(status_switch, key, win):
         win.Find(key).update('Disattivo', text_color='white', background_color='red')
 
 def connection() -> str:
-    # crac_status = CracStatus()
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         win = gui.create_win()
@@ -20,34 +18,36 @@ def connection() -> str:
         while True:
             v, values = win.Read(timeout=2000)
 
+            if not v:
+                s.close()
+                return GuiKey.EXIT
+
             roof = "S"
             curtain_west = "S"
             curtain_east = "S"
 
-            if values:
+            for k, value in values.items():
+                if value:
+                    if k == "RO":
+                        roof = "O"
+                    elif k == "RC":
+                        roof = "C"
+                    elif k == "RS":
+                        roof = "S"
 
-                for k, value in values.items():
-                    if value:
-                        if k == "RO":
-                            roof = "O"
-                        elif k == "RC":
-                            roof = "C"
-                        elif k == "RS":
-                            roof = "S"
+                    elif k == "WO":
+                        curtain_west = "O"
+                    elif k == "WC":
+                        curtain_west = "C"
+                    elif k == "WS":
+                        curtain_west = "S"
 
-                        elif k == "WO":
-                            curtain_west = "O"
-                        elif k == "WC":
-                            curtain_west = "C"
-                        elif k == "WS":
-                            curtain_west = "S"
-
-                        elif k == "EO":
-                            curtain_east = "O"
-                        elif k == "EC":
-                            curtain_east = "C"
-                        elif k == "ES":
-                            curtain_east = "S"
+                    elif k == "EO":
+                        curtain_east = "O"
+                    elif k == "EC":
+                        curtain_east = "C"
+                    elif k == "ES":
+                        curtain_east = "S"
 
             code = roof + curtain_west + curtain_east
 
@@ -59,8 +59,6 @@ def connection() -> str:
 
             data = rcv.decode("UTF-8")
             Logger.getLogger().debug("Data: %s", data)
-            crac_status = CracStatus(data)
-            Logger.getLogger().debug("CRAC_STATUS: %s", crac_status)
 
             # # ROOF
             change_status(data[3], "Roof_open", win)
@@ -74,9 +72,7 @@ def connection() -> str:
             change_status(data[7], "Curtain_E_is_open", win)
             change_status(data[8], "Curtain_E_is_closed", win)
 
-            if not v or v is GuiKey.EXIT or v is GuiKey.SHUTDOWN:
-                s.close()
-                return GuiKey.EXIT
+            
 
         
 
