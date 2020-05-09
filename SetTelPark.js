b91e3b733e5bc4e80034bc2e428d03b0765b161c
Alessio Caradossi <acaradossi@hotels.com>
2020-05-09 18:00:06 +0200

    fix nesting in SetTelPark #90

--- SetTelPark.js
+++ SetTelPark.js
@@ -7,15 +7,14 @@ var Out;
 
 sky6RASCOMTele.Connect();
 
-
 if (sky6RASCOMTele.IsConnected==0)//Connect failed for some reason
 {
 	Out = "Not connected"
 }
 else
 {
-	sky6RASCOMTele.SlewToAzAlt(TargetAz, TargetAlt, "");
-        /*console.log("Slew complete");*/
+  sky6RASCOMTele.SlewToAzAlt(TargetAz, TargetAlt, "");
+  /*console.log("Slew complete");*/
   sky6RASCOMTele.SetTracking(0, 1, 0, 0);
 }
 /* Socket End Packet */
