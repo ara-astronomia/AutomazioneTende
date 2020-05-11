516a0625f95590cbacfb5d7559886c38fff8240d
spider65 <raniero.albanesi@gmail.com>
2020-05-11 17:01:17 +0200

    Added method Is Tracking, to read status tracking

--- MountGetAltAzi.js
+++ MountGetAltAzi.js
@@ -10,9 +10,11 @@ if (sky6RASCOMTele.IsConnected==0)/*Connect failed for some reason*/
 else
 {
 	sky6RASCOMTele.GetAzAlt();
+	sky6RASCOMTele.IsTracking();
 	obj = {
 		az: sky6RASCOMTele.dAz,
-		alt: sky6RASCOMTele.dAlt
+		alt: sky6RASCOMTele.dAlt,
+		tr: sky6RASCOMTele.IsTracking
 	};
 	// Out  = String(sky6RASCOMTele.dAz) +" | " + String(sky6RASCOMTele.dAlt);
 	Out = JSON.stringify(obj);
