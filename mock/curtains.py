ff0b08b4fd1e422d191de714593a471238eb0ec9
spider65 <raniero.albanesi@gmail.com>
2020-06-17 11:16:52 +0200

    added import CurtainsStatus,replaced Status with CurtainsStatus

--- mock/curtains.py
+++ mock/curtains.py
@@ -1,7 +1,7 @@
 import config
 from base.singleton import Singleton
 import threading
-from status import Status
+from status import CurtainsStatus
 
 class Curtain:
     def __init__(self):
@@ -17,7 +17,7 @@ class Curtain:
         """ Reset the steps counter with the help of the edge switchers """
 
         status = self.read()
-        if status != Status.STOPPED and status != Status.DANGER:
+        if status != CurtainsStatus.STOPPED and status != CurtainsStatus.DANGER:
             return
 
         distance_to_min_step = abs(self.steps - self.__min_step__)
@@ -35,14 +35,14 @@ class Curtain:
         status = None
 
         if self.steps == self.__max_step__:
-            status = Status.OPEN
+            status = CurtainsStatus.ACTIVED
         elif self.steps == self.__min_step__:
-            status = Status.CLOSED
+            status = CurtainsStatus.CLOSED
         else:
-            status = Status.STOPPED
+            status = CurtainsStatus.STOPPED
 
         if not status:
-            status = Status.ERROR
+            status = CurtainsStatus.ERROR
 
         return status
 
@@ -75,7 +75,7 @@ class Curtain:
         """ Disable pin motor """
 
         status = None
-        status = Status.STOPPED
+        status = CurtainsStatus.STOPPED
         return status
 
 class WestCurtain(Curtain, metaclass=Singleton):
