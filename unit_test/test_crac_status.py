d8cd6f58ad908aeb7c08354164a2673c0a44f168
spider65 <raniero.albanesi@gmail.com>
2020-06-17 11:17:11 +0200

    added import CurtainsStatus, replaced Status with CurtainsStatus

--- unit_test/test_crac_status.py
+++ unit_test/test_crac_status.py
@@ -1,23 +1,23 @@
 import unittest
 from crac_status import CracStatus
-from status import Status, TelescopeStatus
+from status import Status, TelescopeStatus, CurtainsStatus
 
 class TestCracStatus(unittest.TestCase):
 
     def test_is_in_anomaly(self):
         cs = CracStatus()
-        cs.curtain_east_status = Status.STOPPED
+        cs.curtain_east_status = CurtainsStatus.DEACTIVED
         self.assertTrue(cs.is_in_anomaly())
 
     def test_are_in_danger(self):
         cs = CracStatus()
-        cs.curtain_east_status = Status.DANGER
+        cs.curtain_east_status = CurtainsStatus.DANGER
         self.assertTrue(cs.are_curtains_in_danger())
 
     def test_are_closed(self):
         cs = CracStatus()
-        cs.curtain_east_status = Status.CLOSED
-        cs.curtain_west_status = Status.CLOSED
+        cs.curtain_east_status = CurtainsStatus.DEACTIVED
+        cs.curtain_west_status = CurtainsStatus.DEACTIVED
         self.assertTrue(cs.are_curtains_closed())
 
     def test_telescope_in_secure_and_roof_is_closed(self):
