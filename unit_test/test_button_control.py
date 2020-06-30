d4d4e0110fcf9fa8569499f9bf19498c77d22daf
Alessio Caradossi <acaradossi@hotels.com>
2020-06-30 21:33:54 +0200

    #103 added unit test for button control

---
+++ unit_test/test_button_control.py
@@ -0,0 +1,53 @@
+import unittest
+from unittest.mock import MagicMock, patch
+from components.button_control import ButtonControl
+from gpio_config import GPIOConfig
+from gpio_pin import GPIOPin
+from base.singleton import Singleton
+from status import ButtonStatus
+import time
+import config
+
+
+class TestButtonControl(unittest.TestCase):
+
+    def panel_control(self):
+        return ButtonControl(GPIOPin.SWITCH_PANEL)
+
+    def power_control(self):
+        return ButtonControl(GPIOPin.SWITCH_POWER)
+
+    def light_control(self):
+        return ButtonControl(GPIOPin.SWITCH_LIGHT)
+
+    def aux_control(self):
+        return ButtonControl(GPIOPin.SWITCH_AUX)
+
+    def setUp(self):
+        Singleton._instances = {}
+
+    def test_panel_control(self):
+        self.__check_button__(self.panel_control())
+
+    def test_power_control(self):
+        self.__check_button__(self.power_control())
+
+    def test_light_control(self):
+        self.__check_button__(self.light_control())
+
+    def test_aux_control(self):
+        self.__check_button__(self.aux_control())
+
+    def __check_button__(self, button):
+        button.gpioconfig = MagicMock()
+        button.on()
+        button.gpioconfig.turn_on.assert_called_once()
+        button.off()
+        button.gpioconfig.turn_off.assert_called_once()
+
+    def test_read_button(self):
+        button = self.panel_control()
+        button.gpioconfig.status = MagicMock(return_value=True)
+        self.assertEqual(button.read(), ButtonStatus.ON)
+        button.gpioconfig.status = MagicMock(return_value=False)
+        self.assertEqual(button.read(), ButtonStatus.OFF)
