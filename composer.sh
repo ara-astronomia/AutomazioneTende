96a2eeef05e7039da0a21d4e67eccbca7f4d1339
Alessio Caradossi <acaradossi@hotels.com>
2020-11-11 11:42:12 +0100

    #135 improved composer script

--- composer.sh
+++ composer.sh
@@ -1,23 +1,27 @@
 #! /bin/bash
 
-while getopts o:s: option
+while getopts o:s:a: option
 do
 case "${option}"
 in
-o) OS=${OPTARG};;
+o) OS=${OPTARG};; # mac
 s) SERVICE=${OPTARG};; # start, stop, build
+a) APP=${OPTARG};; #crac_server, crac_client
 esac
 done
 
 if [ "$SERVICE" = "stop" ]; then
-docker-compose stop 
-if [ "$OS" = "MAC" ]; then
+docker-compose stop $APP
+if [ "$OS" = "mac" ]; then
 osascript -e 'quit app "XQuartz"'
 fi
 exit 0
+elif [ "$SERVICE" = "logs" ]; then
+docker-compose logs -f --tail=250 $APP
+exit 0
 fi
 
-if [ "$OS" = "MAC" ]; then
+if [ "$OS" = "mac" ]; then
 open -a XQuartz
 fi
 
@@ -26,7 +30,7 @@ export DISPLAY=$IP:0
 xhost + $IP
 
 if [ "$SERVICE" = "build" ]; then
-docker-compose up -d --build
+docker-compose up -d --build $APP
 else
-docker-compose up -d 
+docker-compose up -d $APP
 fi
