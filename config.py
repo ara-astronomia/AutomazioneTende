50c5c40177322be8d3e98136a735ef2a4ccc6cf4
Alessio Caradossi <acaradossi@hotels.com>
2020-11-16 18:06:31 +0100

    #135 read the value from environment first and then fallback to config.ini

--- config.py
+++ config.py
@@ -13,14 +13,32 @@ class Config(metaclass=Singleton):
     @staticmethod
     def getValue(key, section='automazione'):
         config = Config()
+        env_value = Config.__check_environ__(key, section=section)
+        if env_value:
+            print("Retrieve {key}, {section} from env: {env_value}")
+            return env_value
         return config.configparser[section][key]
 
     @staticmethod
     def getFloat(key, section='automazione'):
         config = Config()
+        env_value = Config.__check_environ__(key, section=section)
+        if env_value:
+            print("Retrieve {key}, {section} from env: {env_value}")
+            return float(env_value)
         return config.configparser[section].getfloat(key)
 
     @staticmethod
     def getInt(key, section='automazione'):
         config = Config()
+        env_value = Config.__check_environ__(key, section=section)
+        if env_value:
+            print("Retrieve {key}, {section} from env: {env_value}")
+            return int(env_value)
         return config.configparser[section].getint(key)
+
+    @staticmethod
+    def __check_environ__(key: str, section='automazione'):
+        env_key = section.upper() + '_' + key.upper()
+        env_value = os.environ.get(env_key)
+        return env_value
