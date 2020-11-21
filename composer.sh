ee3ca73e546c0f96ae9f800f5a192e53292ba8f4
Alessio Caradossi <acaradossi@hotels.com>
2020-11-21 10:02:21 +0100

    #135 server port configuration for docker and better formatting composer.sh

--- composer.sh
+++ composer.sh
@@ -1,31 +1,31 @@
 #! /bin/bash
 
-while getopts o:s:a: option
-do
+while getopts o:s:a: option; do
 case "${option}"
 in
-o) OS=${OPTARG};; # mac
-s) SERVICE=${OPTARG};; # start, stop, build, logs, restart
-a) APP=${OPTARG};; # crac_server, crac_client
-esac
-done
+    o) OS=${OPTARG};; # mac
+    s) SERVICE=${OPTARG};; # start, stop, build, logs, restart
+    a) APP=${OPTARG};; # crac_server, crac_client
+esac; done
+
+echo Eseguo $APP
 
 if [ "$SERVICE" = "stop" ]; then
-docker-compose stop $APP
-if [ "$OS" = "mac" ]; then
-osascript -e 'quit app "XQuartz"'
-fi
-exit 0
+    docker-compose stop $APP
+    if [ "$OS" = "mac" ]; then
+        osascript -e 'quit app "XQuartz"'
+    fi
+    exit 0
 elif [ "$SERVICE" = "logs" ]; then
-docker-compose logs -f --tail=250 $APP
-exit 0
+    docker-compose logs -f --tail=250 $APP
+    exit 0
 elif [ "$SERVICE" = "restart" ]; then
-docker-compose restart $APP
-exit 0
+    docker-compose restart $APP
+    exit 0
 fi
 
 if [ "$OS" = "mac" ]; then
-open -a XQuartz
+    open -a XQuartz
 fi
 
 export IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
@@ -33,7 +33,7 @@ export DISPLAY=$IP:0
 xhost + $IP
 
 if [ "$SERVICE" = "build" ]; then
-docker-compose up -d --build $APP
+    docker-compose up -d --build $APP
 else
-docker-compose up -d $APP
+    docker-compose up -d $APP
 fi
