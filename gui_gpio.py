fd30a44a83f8bf7a700cb292193777eaefbc5e35
spider65 <raniero.albanesi@gmail.com>
2021-07-16 10:38:44 +0200

    correct keys and added required fields

--- gui_gpio.py
+++ gui_gpio.py
@@ -1,55 +1,55 @@
-
 import PySimpleGUI as sg
 from gui_constants import GuiLabel, GuiKey
 
 
-sg.theme('Dark Brown')
+sg.theme('DarkBlue')
 
 
 layout = [
-            [sg.Text('setting parametri CRaC', size=(41,1),font=('Arial',20),justification=('center'))],
+            [sg.Text('SETTING GPIO', size=(41,1),font=('Arial',20),justification=('center'))],
                 [sg.Frame(layout=([
-                    [sg.Text('clk_e', size=(15,1),font=('Arial',9)),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))],
-                    [sg.Text('dt_e', size=(15,1),font=('Arial',9)),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))],
-                    [sg.Text('clk_w', size=(15,1),font=('Arial',9)),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))],
-                    [sg.Text('dt_w', size=(15,1),font=('Arial',9)),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))]
+                    [sg.Text('clk_e', size=(20,1),font=('Arial',9), key ='clk_e'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('dt_e', size=(20,1),font=('Arial',9), key ='dt_e'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('clk_w', size=(20,1),font=('Arial',9), key ='clk_w'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('dt_w', size=(20,1),font=('Arial',9), key ='dt_w'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))]
                     ]),title='Encoder Board', font=('Arial',14), pad=(3, 0)),
+                ],
                 [sg.Frame(layout=([
-                    [sg.Text('motorE_A', size=(15,1),font=('Arial',9), key ='lon'),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))],
-                    [sg.Text('motorE_B', size=(15,1),font=('Arial',9), key ='lat'),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))],
-                    [sg.Text('motorE_E', size=(15,1),font=('Arial',9), key ='hight'),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))],
-                    [sg.Text('motorW_A', size=(15,1),font=('Arial',9), key='ut'),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))],
-                    [sg.Text('motorW_B', size=(15,1),font=('Arial',9), key='dayl'),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))],
-                    [sg.Text('motorW_E', size=(15,1),font=('Arial',9), key='eqx'),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))]
+                    [sg.Text('motorE_A', size=(20,1),font=('Arial',9), key ='motorE_A'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('motorE_B', size=(20,1),font=('Arial',9), key ='motorE_B'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('motorE_E', size=(20,1),font=('Arial',9), key ='motorE_E'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('motorW_A', size=(20,1),font=('Arial',9), key='motorW_A'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('motorW_B', size=(20,1),font=('Arial',9), key='motorW_B'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('motorW_E', size=(20,1),font=('Arial',9), key='motorW_E'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))]
                     ]),title='Motors Board', font=('Arial',14), pad=(3, 0)),
                 sg.Frame(layout=([
-                    [sg.Text('switch_panel', size=(15,1),font=('Arial',9), key ='max_sec_alt'),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))],
-                    [sg.Text('switch_power', size=(15,1),font=('Arial',9), key ='park_az'),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))],
-                    [sg.Text('switch_light', size=(15,1),font=('Arial',9), key ='park_alt'),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))],
-                    [sg.Text('switch_aux', size=(15,1),font=('Arial',9), key ='flat_az'),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))]
+                    [sg.Text('switch_panel', size=(20,1),font=('Arial',9), key ='switch_panel'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('switch_power', size=(20,1),font=('Arial',9), key ='switch_power'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('switch_light', size=(20,1),font=('Arial',9), key ='switch_light'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('switch_aux', size=(20,1),font=('Arial',9), key ='switch_aux'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))]
                     ]),title='Panel Board', font=('Arial',14), pad=(3, 0)),
                 ],
                 [sg.Frame(layout=([
-                    [sg.Text('roof_verify_closed', size=(15,1),font=('Arial',9), key ='Az_NE'),sg.InputText('fsaff', size=(25,1), font=('Arial',9), text_color=('black'))],
-                    [sg.Text('roof_verify_open', size=(15,1),font=('Arial',9), key ='Az_SE'),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))],
-                    [sg.Text('switch_roof', size=(15,1),font=('Arial',9), key ='Az_SW'),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))],
-                    [sg.Text('wait_for_timeout', size=(15,1),font=('Arial',9), key ='Az_NW'),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))]
-                    ]),title='event_bouncetime', font=('Arial',14), pad=(3, 0)),
+                    [sg.Text('roof_verify_closed', size=(20,1),font=('Arial',9), key ='roof_ver_closed'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('roof_verify_open', size=(20,1),font=('Arial',9), key ='roof_ver_open'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('switch_roof', size=(20,1),font=('Arial',9), key ='switch_roof'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('wait_for_timeout', size=(20,1),font=('Arial',9), key ='wait_timeout'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('event_bouncetime', size=(20,1),font=('Arial',9), key ='bouncetime'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))]
+                    ]),title='Roof Board', font=('Arial',14), pad=(3, 0)),
                 sg.Frame(layout=([
-                    [sg.Text('curtain_W_verify_open', size=(15,1),font=('Arial',9)),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))],
-                    [sg.Text('curtain_W_verify_closed', size=(15,1),font=('Arial',9), key ='max_step'),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))],
-                    [sg.Text('curtain_E_verify_open', size=(15,1),font=('Arial',9), key ='max_secure_step'),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))],
-                    [sg.Text('curtain_E_verify_closed', size=(15,1),font=('Arial',9), key ='min_step'),sg.InputText('', size=(25,1), font=('Arial',9), text_color=('black'))]
-                    ]),title=' Curtains Limits Switch', font=('Arial',14), pad=(3, 0)),
+                    [sg.Text('curtain_W_verify_open', size=(20,1),font=('Arial',9), key ='w_ver_open'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('curtain_W_verify_closed', size=(20,1),font=('Arial',9), key ='w_ver_close'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('curtain_E_verify_open', size=(20,1),font=('Arial',9), key ='e_ver_open'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))],
+                    [sg.Text('curtain_E_verify_closed', size=(20,1),font=('Arial',9), key ='e_ver_close'),sg.InputText('', size=(10,1), font=('Arial',9), text_color=('white'))]
+                    ]),title='Curtains Limits Switch', font=('Arial',14), pad=(3, 0)),
                 ],
-                [[sg.Button(Ok)],
-                [sg.Button('Exit')]],
-             ]
+                [sg.Frame(layout=([
+                    [sg.Button("Read"), sg.Button("Write"), sg.Button('Exit')]
+                    ]),title=''),
+                ],
+            ]
 
-        #[sg.Listbox(values=sg.theme_list(), size=(20, 15), key='-LIST-', enable_events=True)],
-        #[sg.Button('Ok')]]
-
-window = sg.Window('Setting CRaC', layout, grab_anywhere=False, finalize=True)
+window = sg.Window ('Setting CRaC -- Control Roof and Curtains by ARA', layout, grab_anywhere=False, finalize=True)
 
 while True:  # Event Loop
     event, values = window.read()
